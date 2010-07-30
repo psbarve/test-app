93c4a32443bceb07165dc29c73ec93f9f860a8ad
psbarve <psbarve@gmail.com>
2010-07-30 15:04:44 +0900

    First Ruby Prog

--- ruby/first.rb
+++ ruby/first.rb
@@ -5,3 +5,19 @@ puts "Test Times" * 3
 5.times { print "Times" }
 
 puts
+
+require "rexml/document"
+file = File.new("mesg.xml")
+doc = REXML::Document.new file
+puts doc.to_s
+
+require "tk"
+test = TkRoot.new
+TkLabel.new(test)do
+	text "\n Test Message for new Window \n"
+	pack
+end
+Tk.mainloop
+
+
+
